b183a5e80fbcbd31751d5ea8e42cbaa443448cf8
ToNaza <misterket37@gmail.com>
2025-03-15 04:03:06 +0200

    mini upgrate or prof

---
+++ prof/script.js
@@ -0,0 +1,6 @@
+document.getElementById("openModal2").addEventListener("click", function() {
+    document.getElementById("modal2").style.display = "flex";
+});
+document.getElementById("modal2").addEventListener("click", function() {
+    document.getElementById("modal2").style.display = "none";
+});
